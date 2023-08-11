765d2b7c56a8c90fc0ab187a1bdb4d4aaa7e1e6a
Kashof1 <theibster6@gmail.com>
2023-08-11 17:08:30 +0100

    minor updates

--- core/scraper.py
+++ core/scraper.py
@@ -1,21 +1,26 @@
-from utils import get_url
+from core.utils import get_url
 import logging
-import tfl
+import core.tfl as tfl
 import time
+from core.databaseutils import DatabaseHandler
 
 log = logging.getLogger(__name__)
 tflinstance = tfl.get_tfl()
 
-class scraper:
-    def __init__(self, linename: str):
-        log.info("LOADED SCRAPER INSTANCE")
+class Scraper:
+    def __init__(self, linename: str, filename: str):
+        log.info(f"LOADED {linename} SCRAPER INSTANCE")
         self.line = linename
-        
+        self.destinationfile = filename
+        self.databasehandler = DatabaseHandler(self.destinationfile)
     
     def scrape(self):
-        while True:
-            data = tflinstance.get_tubedata(self.line) 
-            """to do - loop and call data every 30 seconds from API.
-            create a mock database handling class (just text file read/writing for now) and call this here 
-            in order to append the data to the text file every time it's been called.
-            Add a method to killing the program."""
+        data = tflinstance.get_tubedata(self.line) 
+        self.databasehandler.write(data)
+
+
+
+    """to do - loop and call data every 30 seconds from API.
+    create a mock database handling class (just text file read/writing for now) and call this here 
+    in order to append the data to the text file every time it's been called.
+    Add a method to killing the program."""
--- scrapebuilder.py
+++ scrapebuilder.py
@@ -1,7 +1,7 @@
 from core.scraper import Scraper
 import time
 
-jubileescraper = Scraper('jubilee', 'jubileetest.txt')
+jubileescraper = Scraper('jubilee', 'linedatabases/raw/jubileetest.txt')
 jubileescraper.scrape()
 print ('first scrape successful')
 time.sleep(30)
