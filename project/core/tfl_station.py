794f4d0c97bd49e1e094e7d96a5eda0b25e003f6
Kashof1 <theibster6@gmail.com>
2023-10-27 14:10:54 +0100

    unsuccesful attempts at resolving import isssues

--- project/core/tfl_station.py
+++ project/core/tfl_station.py
@@ -8,7 +8,9 @@ from core.tfl_line import get_tflline
 
 log = logging.getLogger(__name__)
 
-directory = os.path.join('project','data','')
+directory = os.path.join('project','data','stations.json')
+with open (directory, 'r') as file:
+    print (file)
 
 class get_tflstation():
     #station name (for convenience) paired with naptanid needed for API call
