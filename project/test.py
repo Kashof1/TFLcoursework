288863b73277b553f85f09c74656cdec77386982
Kashof1 <theibster6@gmail.com>
2023-11-16 20:22:34 +0000

    untested mongo implementation in train tracker

--- project/test.py
+++ project/test.py
@@ -1,9 +1,11 @@
-'''TO DO: 
+"""TO DO: 
 -When a train arrives at its station, that vehicleId is removed from the dataset (i.e. no longer found)
 -find a way to loop the code to repeatedly call new data from API (every 30 seconds).
--when a train has had its ActualTime field filled, remove it from the dictionary and put the information into a separate file'''
+-when a train has had its ActualTime field filled, remove it from the dictionary and put the information into a separate file"""
 from datetime import datetime, timedelta
 import time
+
+import pymongo
 
 from core.tfl_station import get_tflstation
 
@@ -12,14 +14,35 @@ tfl_api = get_tflstation()
 line = 'jubilee'
 station = 'Stratford Underground Station'
 
-'''Currently developing for Jubilee at Stratford; need to ensure that code works for any line at any station before implementing
-multiple stations and lines at once'''
+"""Currently developing for Jubilee at Stratford; need to ensure that code works for any line at any station before implementing
+multiple stations and lines at once"""
 
-'''data[0]['currentLocation'] - sample index for data'''
+"""data[0]['currentLocation'] - sample index for data"""
+
+dbclient = pymongo.MongoClient("mongodb://localhost:27017/")
+db = dbclient["TFLData"]
+
+existingCols = db.list_collection_names()
+colName = f"{line}-{station.replace(' ', '+')}-col" #replacing the spaces with + for ease of referencing
+
+if colName not in existingCols:
+    db.createCollection(
+        colName,
+        {
+            timeseries: {
+                timeField: 'time',
+                metaField: 'meta',
+                granularity: 'seconds'
+            }
+        } )
+    
+currentCol = db[colName] 
 
 currentTrains = {
     "TrainID" : ['PredictedTime', 'ActualTime', 'Difference']
 }
+
+
 
 while True:
     data = tfl_api.get_tubedata(line=line, station=station) 
@@ -32,25 +55,34 @@ while True:
             formattedPrediction = datetime.strptime(each['expectedArrival'], '%Y-%m-%dT%H:%M:%SZ')
             currentTrains[trainId] = [formattedPrediction,'','']
 
-    currentTrainIterator = iter(currentTrains) #iterator used to skip the first item in currentTrains, which serves as a header (potential issues here)
+    currentTrainIterator = iter(currentTrains) #iterator used to skip the first item in currentTrains, which serves as a header
     next(currentTrainIterator)
     for currentTrainid in currentTrainIterator:
-        if not any(dataLine['vehicleId'] == currentTrainid for dataLine in data): 
+        if not any(dataLine['vehicleId'] == currentTrainid for dataLine in data): #if trainid no longer in api data, then train has reached station
             eachArray = currentTrains[currentTrainid]
-            predictedTime = eachArray[0] 
+            predictedTime = datetime(eachArray[0]) 
             actualTime = datetime.now().replace(microsecond=0)
 
             #calculating difference in times (in seconds)
             #positive difference --> late train, negative difference --> early train
             difference = (actualTime - predictedTime).total_seconds()
             
-            eachArray[1] = actualTime
-            eachArray[2] = difference
-            currentTrains[each['vehicleId']] = eachArray
+            metavals = { #could investigate more metadata to be used
+                'predictedTime' : predictedTime.isoformat(), 
+                'actualTime' : actualTime.isoformat(),
+                'line' : line,
+                'station' : station.replace(' ','+')
+                }
+            timeval = actualTime.isoformat()
+            db.colName.insert_one({
+                'meta' : metavals,
+                'time' : timeval,
+                'timediff' : difference
+            })
 
 
     print (currentTrains)
     print ('*' * 90)
-    time.sleep(5) #each train needs to get popped out into a separte file and deleted from here you fucking idiot thanks
+    time.sleep(5) #each train needs to get popped out into a separate file and deleted from here you fucking idiot thanks
     # trains that are super early could be being cancelled
 
