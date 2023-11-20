94748cb956cf2173a5ba5342f5ff77b71a8570f3
Kashof1 <theibster6@gmail.com>
2023-11-20 18:41:47 +0000

    tested train removal from dict (working)

--- project/test.py
+++ project/test.py
@@ -1,5 +1,4 @@
 """TO DO: 
--remove trains that have been found from dictionary
 -investiage time format for mongosh, and unify this so all time data is same format
 -filter out trains that are being cancelled (absurdly early)
 -find other metadata that could be used
