972575a84398223bf84926cefef7cf7e4b6ba61b
Kashof1 <theibster6@gmail.com>
2023-12-11 19:57:04 +0000

    disruptions page added for testing

--- project/main.py
+++ project/main.py
@@ -3,8 +3,7 @@ import os
 from pathlib import Path
 from fastapi import FastAPI
 from core.tfl_line import get_tflline
-from core.tfl import get_tflstation
-from core.tfl import get_crowdingdata
+from core.tfl import get_tflstation, get_crowdingdata, get_disruptionstatus
 logs_file = Path(Path().resolve(), "log.txt")
 logs_file.touch(exist_ok=True)
 
@@ -20,6 +19,7 @@ log = logging.getLogger(__name__)
 app = FastAPI()
 station_train_api = get_tflstation()
 station_crowd_api = get_crowdingdata()
+line_disruption_api = get_disruptionstatus()
 
 @app.get('/')
 async def root():
@@ -47,3 +47,9 @@ async def getstationcrowdingdata(stationName):
     output = station_crowd_api.get_data(station = stationName)
     print (output['percentageOfBaseline'])
     return {"data" : output}
+
+@app.get('/Line/{line}/Disruption')
+async def getarrivalsdata(line):
+    log.info(f"Loaded {line} disruptions data page")
+    output = line_disruption_api.get_data(line=line)
+    return {"data":output}
