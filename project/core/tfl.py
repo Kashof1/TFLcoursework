610b68c4d31f80970b52390f6c5a6deacc1592f0
Kashof1 <theibster6@gmail.com>
2023-12-11 18:51:32 +0000

    minor

--- project/core/tfl.py
+++ project/core/tfl.py
@@ -41,7 +41,7 @@ class get_tflstation():
         return (stationID, line) if valid == True else ('','') 
     
 """
-confirm what type of oop construct is best used here
+confirm what type of oop construct is best used here (done)
 ALSO PENDING TESTING
 """
 
