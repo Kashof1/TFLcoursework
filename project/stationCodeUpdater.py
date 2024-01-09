3f11bafb1ecd488b282e70603fb596d091c1f849
Kashof1 <theibster6@gmail.com>
2024-01-09 19:03:57 +0000

    created working code to gen geojson file
    geojson file has all stations and metadata

--- project/stationCodeUpdater.py
+++ project/stationCodeUpdater.py
@@ -1,7 +1,12 @@
-'''This script updates the json file of all of the common station names and their associated naptanIDs'''
+'''script used to create and manage all the data in the 'data' file, with mulitple different functions
+that all manage and create/update different files in the folder.
 
+This file is not intended to be called or used by any other file, and simply contains maintenance scripts.'''
+
+import csv
 import json
 import os
+from geojson import Feature, FeatureCollection, Point, dump
 
 from core.utils import get_url
 
@@ -38,10 +43,30 @@ def stationLineCombinationUpdater():
     with open (savepath, 'w') as outputfile:
         json.dump(final, outputfile)
 
+def stationLocationJson():
+    featurearray = []
+    sourceFilepath = os.path.join('data','stationLocRaw.csv')
+    destFilepath = os.path.join('data', 'stationsgeo.json')
+    with open (sourceFilepath, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for count, each in enumerate(reader):
+            if each['NETWORK'] == 'London Underground':
+                geometry = Point(coordinates=(float(each['y']), float(each['x']))) #(y,x) as we are using lat, long
+                properties = {
+                    "name" : each['NAME'],
+                    "linesServed" : each['LINES'],
+                    "zone" : each['Zone']
+                }
+                featurearray.append(Feature(geometry = geometry, properties = properties))
+    featurecollection = FeatureCollection(featurearray)
+    with open (destFilepath, 'w') as destination:
+        dump(obj=featurecollection, fp=destination)
+    
 
 
 if __name__ == '__main__':
-    while True:
+    stationLocationJson()
+    '''while True:
         decision = int(input('Enter 1 to update the stations and their naptanIDs, enter 2 to update the station-line combinations, enter 3 to break'))
         if decision == 1:
             stationNaptanUpdater()
@@ -52,4 +77,4 @@ if __name__ == '__main__':
         elif decision == 3:
             break
         else:
-            print ('Invalid input, try again')
+            print ('Invalid input, try again')'''
