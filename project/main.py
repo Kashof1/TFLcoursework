e4734a3b39fadebd4c30c00a56b332772449fe5b
Kashof1 <theibster6@gmail.com>
2024-03-15 07:40:47 +0000

    currently duplicating drop downs in the offcanvas for lines from prev seletctions

--- project/main.py
+++ project/main.py
@@ -58,12 +58,19 @@ def mappage(request: Request):
 @app.post('/', response_class=HTMLResponse)
 def get_markerStationResponse(request: Request, markerresponse : MarkerResponse):
     returnedStation = markerresponse.station
+    stationName = f'{returnedStation} Underground Station'
+    with open (os.path.join('data', 'stationLineCombos.json'), 'r') as f:
+        stationdata = json.load(f)
+    linesServed = [each[0] for each in stationdata if each[1] == stationName]
+
     data = {
-        "station": returnedStation
+        "station": stationName,
+        "linesServed" : linesServed
         }
     log.info(f'Server received marker click data for {data}')
 
     encoded = jsonable_encoder(data)
+    log.info(encoded)
     return JSONResponse(content = encoded)
     
 
