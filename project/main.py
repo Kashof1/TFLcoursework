7e63592b9ac3ab5cf1fb7154ca5e9db236eb47d3
Kashof1 <theibster6@gmail.com>
2024-03-17 14:15:57 +0000

    added code that sends the next arrivals data for each unique destination to frontend

--- project/main.py
+++ project/main.py
@@ -11,7 +11,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
-from core.tfl import get_tflstation, get_crowdingdata, get_statusseverity
+from core.tfl import get_tflstation
 
 logs_file = Path(Path().resolve(), "log.txt")
 logs_file.touch(exist_ok=True)
@@ -29,9 +29,7 @@ app = FastAPI()
 app.mount('/static', StaticFiles(directory='templates/static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
-station_train_api = get_tflstation()
-station_crowd_api = get_crowdingdata()
-line_disruption_api = get_statusseverity()
+next_trains_api = get_tflstation()
 
 class MarkerResponse(BaseModel):
     station: str
@@ -61,11 +59,18 @@ def get_markerStationResponse(request: Request, markerresponse : MarkerResponse)
     stationName = f'{returnedStation} Underground Station'
     with open (os.path.join('data', 'stationLineCombos.json'), 'r') as f:
         stationdata = json.load(f)
-    linesServed = [each[0].capitalize() for each in stationdata if each[1] == stationName] #error for hyphenated stations
+    linesServed = [each[0].capitalize() for each in stationdata if each[1] == stationName] 
+    stationName = stationName.title()
+
+    arrivalsDict = {}
+    for line in linesServed:
+        lineArrivals = next_trains_api.get_next_unique_trains(station=stationName, line=line)
+        arrivalsDict[line] = lineArrivals
 
     data = {
-        "station": stationName.title(),
-        "linesServed" : linesServed
+        "station": stationName,
+        "linesServed" : linesServed,
+        'nextArrivals' : arrivalsDict
         }
     log.info(f'Server received marker click data for {data}')
 
@@ -92,28 +97,10 @@ def return_stationGeoData(request: Request):
     return JSONResponse(content = encoded)
 
     
-@app.get('/{line}/Arrivals/{station}')
-async def getstationarrivalsdata(line, station):
-    station = station.replace('+',' ')
-    log.info(f"Loaded {line} arrivals page for {station}")
-    output = station_train_api.get_data(line, station)
-    return {"data" : output}
 
 
-@app.get('/Crowding/{stationName}/Live')
-async def getstationcrowdingdata(stationName):
-    stationName = stationName.replace('+',' ')
-    log.info(f"Loaded crowding page for {stationName}")
-    output = station_crowd_api.get_data(station = stationName)
-    print (output['percentageOfBaseline'])
-    return {"data" : output}
 
 
-@app.get('/Line/{line}/Disruption')
-async def getarrivalsdata(line):
-    log.info(f"Loaded {line} disruptions data page")
-    output = line_disruption_api.get_data(line=line)
-    return {"data":output}
 
 
 @app.get('/testpage')
