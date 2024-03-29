f50cc616a86b327be908b1f94a5c7de2ead0af39
Kashof1 <theibster6@gmail.com>
2024-03-29 21:26:24 +0000

    new script for further training and subsequent exporting of the best model found by the hyperparameter tuner

---
+++ project/machine_learning/furtherModelTraining.py
@@ -0,0 +1,59 @@
+"""
+this program takes the best model found by the hyperparameter tuner, and retrains a model for longer using those hyperparameters.
+it then saves the trained model in a lightweight format, ready for quick inference
+"""
+
+import datetime
+import os
+
+import keras
+import keras_tuner as kt
+from hyperparamFinder import input_layers_builder, myHyperModel
+from keras import backend as K
+
+(
+    raw_input_layers,
+    encoded_input_layers,
+    training_dataset,
+    testing_dataset,
+    validating_dataset,
+) = input_layers_builder()
+
+model_building_space = myHyperModel(
+    encoded_input_layers=encoded_input_layers, raw_input_layers=raw_input_layers
+)
+tuner = kt.Hyperband(
+    hypermodel=model_building_space,
+    objective="val_mean_absolute_error",
+    max_epochs=20,
+    directory="machine_learning/hyperTuning",
+    project_name="functional_model_Hyperband",
+    overwrite=False,
+)
+
+log_dir = "machine_learning/logs/fit/" + datetime.datetime.now().strftime(
+    "%Y%m%d-%H%M%S"
+)
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0)
+
+early_callback = keras.callbacks.EarlyStopping(
+    monitor="val_loss",
+    patience=2,
+)
+
+besttrial = tuner.oracle.get_best_trials()[0]
+model = tuner.hypermodel.build(besttrial.hyperparameters)
+
+model.fit(
+    training_dataset,
+    epochs=15,
+    callbacks=[early_callback, tensorboard_callback],
+    validation_data=validating_dataset,
+)
+
+[(x, y)] = testing_dataset.take(1)
+predictions = model.predict(x)
+y = list(y)
+predictions = list(predictions)
+for each in range(len(predictions)):
+    print(predictions[each], y[each])
